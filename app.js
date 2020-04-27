7d2ac3c1dd36ab2ac7d3584cacea2cb67e848555
Amulya <amulyavarshney67.av@gmail.com>
2020-04-27 17:05:48 +0530

    now it must work

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ var request=require('request');
 	console.log("ERROR", err.message);
 });
 
+//mongoose.connect('mongodb://localhost:27017/covid_app',{useNewUrlParser:true,useUnifiedTopology:true})  
 
 //,{useNewUrlParser:true,useUnifiedTopology:true})
 var covidSchema=new mongoose.Schema({
@@ -61,33 +62,34 @@ var Covid=mongoose.model("Covid",covidSchema);
 var client = new twilio('AC81bad57961d2f256bae9d7d47a17975a','eb17dc86afbc9181ff3e3b819372779e');
 
 //bs deploy ke lye hai baad me delete krna hai
-Covid.create({
-		 	name:"Aligarh",
-			confirmed:11
-		  },function(err,response){
-		 	if(err){
-		 		console.log(err)
-			 	 }
-			 	 else{
-			 	 	console.log("successfully created")
-			 	 }
-			 });
+// Covid.create({
+// 		 	name:"Aligarh",
+// 			confirmed:11
+// 		  },function(err,response){
+// 		 	if(err){
+// 		 		console.log(err)
+// 			 	 }
+// 			 	 else{
+// 			 	 	console.log("successfully created")
+// 			 	 }
+// 			 });
 
 //var a;
-var job=new CronJob('* * * * * *',function(){
+var job=new CronJob('* * * * *',function(){
 	var options = {
 			method: 'GET',
 			url: 'https://covid-19-india-data-by-zt.p.rapidapi.com/GetIndiaDistrictWiseDataForState',
 			qs: {statecode: 'UP'},
 			headers: {
 				'x-rapidapi-host': 'covid-19-india-data-by-zt.p.rapidapi.com',
-				'x-rapidapi-key': '1c138af3edmsh5f4bf9a1aad505cp1a750ejsn07d6bdb74696'
+			    'x-rapidapi-key': 'bf75e041c8msh13265b677eb9845p164888jsn89f3a2ef44f1'
 			}
 	};
 request(options, function (error, response, body) {
 	if (error) throw new Error(error);
 	else{
 		var parsedData=JSON.parse(body);
+		//console.log(body);
 		parsedData.data.filter(function(city){
 		 	if(city.name=="Aligarh"){
 		 	//console.log(city.confirmed + " " + typeof city.confirmed);

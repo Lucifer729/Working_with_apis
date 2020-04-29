8b59a810375944d753c9e1e48cb9b5f920db6fa8
Amulya <amulyavarshney67.av@gmail.com>
2020-04-29 16:57:17 +0530

    new no added

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var request=require('request');
 // });
 
 //var url = process.env.MONGOLAB_URI;
-var arr=['+918630757295','+918077877966','+918619076613','+919456667451'];
+var arr=['+918630757295','+918077877966','+918619076613','+919456667451','+919719244755'];
 
 const myPingConfig = {
   appName: "myfirstdeployy",

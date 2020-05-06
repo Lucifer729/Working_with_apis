e4d4dd09f5989cb51be0c0b2930fffe60a9f8e34
Amulya <amulyavarshney67.av@gmail.com>
2020-05-06 22:00:10 +0530

    change cron time

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ var client = new twilio('AC81bad57961d2f256bae9d7d47a17975a','eb17dc86afbc9181ff
 // 			 });
 
 //var a;
-var job=new CronJob('*/45 * * * *',function(){
+var job=new CronJob('*/25 * * * *',function(){
 	// var options = {
 	// 		method: 'GET',
 	// 		url: 'https://covid-19-india-data-by-zt.p.rapidapi.com/GetIndiaDistrictWiseDataForState',
